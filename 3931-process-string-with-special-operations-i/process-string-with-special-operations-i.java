1554d7e93b17cde4858b1f9d1fe30305864d66da
YashMoundekar <yashmoundekar9@gmail.com>
2026-06-16 22:34:49 +0530

    Time: 4 ms (76.47%) | Memory: 55.7 MB (20.59%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String processStr(String s) {
+
+        StringBuilder str=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)>='a' && s.charAt(i)<='z'){
+                str.append(s.charAt(i));
+            }
+            else if(s.charAt(i)=='#'){
+                str.append(str.toString());
+            }else if(s.charAt(i)=='%'){
+                str.reverse();
+            }else if(s.charAt(i)=='*'){
+                if (str.length() > 0) {
+                    str.deleteCharAt(str.length() - 1);
+                }
+            }
+        }
+        return str.toString();
+    }
+}
