d0f68bae4340e7971588a99c0cedbcbc385e7f31
YashMoundekar <yashmoundekar9@gmail.com>
2026-06-29 16:05:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (52.26%) - LeetSync

---
+++ 3511-find-the-winning-player-in-coin-game/find-the-winning-player-in-coin-game.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String winningPlayer(int x, int y) {
+        
+        int res=y-x;
+        int min=Math.min(x,y/4);
+
+        return min%2==0?"Bob":"Alice";
+    }
+}
